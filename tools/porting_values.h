48d5a5431fb5fcf8a7be8c2a6194713ae296ccba
sishuo <sishuo@word-tale.com>
2025-10-04 10:44:06 +0800

    upload

---
+++ tools/porting_values.h
@@ -0,0 +1,18 @@
+/* values.h replacement for macOS */
+#ifndef VALUES_H
+#define VALUES_H
+
+#include <limits.h>
+#include <float.h>
+
+/* Define constants that were in the old values.h */
+#define MAXINT INT_MAX
+#define MININT INT_MIN
+#define MAXLONG LONG_MAX
+#define MINLONG LONG_MIN
+#define MAXFLOAT FLT_MAX
+#define MINFLOAT FLT_MIN
+#define MAXDOUBLE DBL_MAX
+#define MINDOUBLE DBL_MIN
+
+#endif /* VALUES_H */
